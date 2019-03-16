96957506c6ec1f9e30665338b4c6b0da9a88d54a
Seamus Bonner <seamusb99@gmail.com>
2019-03-16 23:48:41 +0000

    wss

--- src/frontend/my-app/src/app/input-form/input-form.component.ts
+++ src/frontend/my-app/src/app/input-form/input-form.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ConnectionData } from '../connection_data'
 
-let wsUrl = "ws://" + location.hostname + ":" + location.port;
-let ws = new WebSocket(wsUrl);
+let wsUrl = "wss://" + location.hostname + ":" + location.port;
+let wss = new WebSocket(wsUrl);
 
 @Component({
   selector: 'app-input-form',
@@ -18,7 +18,7 @@ export class InputFormComponent implements OnInit {
   onSubmit(messageString) {
     this.submitted = true;
     console.log("-> tx " + JSON.stringify(messageString));
-    ws.send(JSON.stringify(messageString));
+    wss.send(JSON.stringify(messageString));
   }
 
   get diagnostic() { return JSON.stringify(this.model); }
--- src/static_files/script.js
+++ src/static_files/script.js
@@ -3,12 +3,9 @@
 //                              WebSocket Client
 //----------------------------------------------------------------------------
 
-let wsUrl = "ws://" + location.hostname + ":" + location.port;
+let wsUrl = "wss://" + location.hostname + ":" + location.port;
 let ws = new WebSocket(wsUrl);
 
-ws.onopen = function() {
-  // Do nothing
-}
 
 ws.onclose = function() {
   alert("WebSocket closed. Please reload the page.");
@@ -21,7 +18,7 @@ ws.onerrer = function(e) {
 ws.onmessage = function(m) {
   let messageString = m.data;
   console.log("<- rx " + messageString);
-  let message = JSON.parse(messageString);
+  let message = JSON.parse(messageString);1
   handleMessage(message);
 }
 
