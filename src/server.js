739609ea4c90a9021896878551dbce7ab7ce6464
RyanGibb <ryangibb321@gmail.com>
2019-03-16 14:35:31 +0000

    tweaked server.js and added package.json

--- src/server.js
+++ src/server.js
@@ -1,15 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-// var cors = require('cors')
-// var session = require('express-session');
-// let mysql = require("mysql")
-let app = express();
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+const bodyParser = require('body-parser');
+
+//TODO add this to websocket lol
+//bodyParser.urlencoded({ extended: true})
+//bodyParser.json()
+
+
+
+//----------------------------------------------------------------------------
+//                              HTTP Server
+//----------------------------------------------------------------------------
+
+const express = require('express');
+const http = require('http');
+const httpServer = http.createServer(app);
+
+httpServer.listen(port, function () {
+  console.log('Listening for HTTP requests on port: ' + port);
+});
+
+let app = express();
 
 var server = app.listen(5000, () => {
     console.log('Server is running..');
@@ -18,42 +28,48 @@ var server = app.listen(5000, () => {
 // Serve static files, such as css and scripts, from the directory below.
 app.use(express.static(__dirname + '/static_files'));
 
-// Parse application/x-www-form-urlencoded
-// Don't worry about this.
-app.use(bodyParser.urlencoded({ extended: true}));
+//----------------------------------------------------------------------------
+//                              WebSocket Server
+//----------------------------------------------------------------------------
 
-// Parse application/json
-// Allows us to read JSON.
-app.use(bodyParser.json());
+const ws = require('ws');
 
-// Use this as an example of a super simple GET request.
-// req: The request that has came in, contains all the information from the client to get a page.
-// res: The response that will be set to the client.
-app.get('/', function (req, res) {
+const maxLogMessageLength = 200;
 
-    // Tells the client that it's receiving a html file
-    res.setHeader("Content-Type", "text/html")
+const wsServer = new ws.Server({server: httpServer});
 
-    // The data that is getting sent to the file.
-    res.sendfile('static_files/index.html')
-    return res;
+wsServer.on('connection', function(ws, req) {
+  console.log('WS connection ' + req.connection.remoteAddress + ':'
+      + req.connection.remotePort);
+
+  ws.on('close', function(code, message) {
+    console.log('WS disconnection ' + ws._socket.remoteAddress + ':'
+        + req.connection.remotePort + ' Code ' + code);
+  });
+
+  ws.on('message', function(data) {
+    let messageString = data.toString();
+    console.log('WS -> rx ' + req.connection.remoteAddress + ':' + req.connection.remotePort + ' ' +
+        (messageString.length > maxLogMessageLength ? messageString.slice(0, maxLogMessageLength) + "..." : messageString)
+    );
+
+    try {
+      var receivedMessage = JSON.parse(messageString);
+    }
+    catch(error) {
+      respondError(ws, req, 'error parsing JSON request', error);
+      return;
+    }
+
+    // TODO process message
+
+  })
 });
 
-// Register a new user account to the system, allowing for the user to configure their microbit settings.
-// Use this as an example of how Post commands work.
-app.post('/register', function (req, res) {
-
-    let stored = "Not Stored";
-
-    // Query the database, inserting the given details into the user table.
-    // req.body.username is the data that hsa been sent in the post request from the client. This is how it is accessed.
-    runSQL("INSERT INTO user(username, password, notification_interval) VALUES (?, ?, ?)", [req.body.username, req.body.password, req.body.notification_interval], function (err, recordset) {
-
-        if(err)
-            console.log(err);
-
-        stored = "Stored";
-    });
-
-    res.send(stored);
-});
+function respond(ws, req, responseMessage) {
+  var messageString = JSON.stringify(responseMessage);
+  ws.send(messageString);
+  console.log('WS <- tx ' + req.connection.remoteAddress + ':' + req.connection.remotePort + ' ' +
+    (messageString.length > maxLogMessageLength ? messageString.slice(0, maxLogMessageLength) + "..." : messageString)
+  );
+};
