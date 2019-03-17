eae761fdccb3ea4634ff9fcdc04baa2c25de67e1
RyanGibb <ryangibb321@gmail.com>
2019-03-17 08:19:00 +0000

    removed static audio to text

--- src/server.js
+++ src/server.js
@@ -192,36 +192,6 @@ function hangup(uuid, callback) {
 //                             Google Speech to Text
 //----------------------------------------------------------------------------
 
-// async function speechToText() {
-//
-//   // The name of the audio file to transcribe
-//   const fileName = 'audio.raw';
-//
-//   // Reads a local audio file and converts it to base64
-//   const file = fs.readFileSync(fileName);
-//   const audioBytes = file.toString('base64');
-//
-//   // The audio file's encoding, sample rate in hertz, and BCP-47 language code
-//   const audio = {
-//     content: audioBytes,
-//   };
-//   const config = {
-//     encoding: 'LINEAR16',
-//     sampleRateHertz: 16000,
-//     languageCode: 'en-UK',
-//   };
-//   const request = {
-//     audio: audio,
-//     config: config,
-//   };
-//
-//   // Detects speech in the audio file
-//   const [response] = await client.recognize(request);
-//   const transcription = response.results
-//     .map(result => result.alternatives[0].transcript)
-//     .join('\n');
-//   console.log(`Transcription: ${transcription}`);
-// }
 //
 // var WebSocketServer = require('websocket').server;
 // var url = require("url");
