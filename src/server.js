b75fe8b3d49e0fb26602d4cd950a2215b7df29b5
RyanGibb <ryangibb321@gmail.com>
2019-03-17 08:12:53 +0000

    commented out speech to text

--- src/server.js
+++ src/server.js
@@ -222,129 +222,129 @@ function hangup(uuid, callback) {
 //     .join('\n');
 //   console.log(`Transcription: ${transcription}`);
 // }
-
-var WebSocketServer = require('websocket').server;
-var url = require("url");
-const fs = require('fs');
-const Speech = require('@google-cloud/speech');
-const speech = new Speech.SpeechClient();
-
-//Create a server
-var httpServerNexmo = http.createServer(
-  function (req, res) {
-    // do nothing
-    // res.writeHead(200, {'Content-Type': 'text/plain'});
-    // res.write('Hello World!');
-    // res.end();
-  }
-);
-
-httpServerNexmo.listen(port + 1, function () {
-  console.log('httpServerNexmo on port: ' + (port + 1));
-});
-
-var nexmows = new WebSocketServer({
-    httpServer: httpServerNexmo,
-    autoAcceptConnections: true,
-});
-
-// Receive Recording
-app.post(EVENT_PATH_RECORDING, function(req, res) {
-    var parsedUrl = url.parse(req.url, true); // true to get query as object
-    var getparams = parsedUrl.query;
-    var params = JSON.parse(req.body);
-    console.log(req.body)
-    var localfile = "files/"+params['conversation_uuid']+".wav"
-    nexmo.files.save(params['recording_url'], localfile, (err, response) => {
-      if(response) {
-          console.log('The audio is downloaded successfully!');
-          var response = {text: "http://https://sound-machine-234713.appspot.com/" + localfile,
-                          languageCode: getparams.langCode,
-                          user: getparams.from
-                          }
-      }
-    });
-    res.writeHead(204);
-    res.end();
-});
-
-// Nexmo Websocket Handler
-nexmows.on('connect', function(connection) {
-    console.log((new Date()) + ' Connection accepted' + ' - Protocol Version ' + connection.webSocketVersion);
-    // Create the stream at the start of the call
-    var recognizeStream = new RecognizeStream(connection);
-});
-
-class RecognizeStream {
-    constructor(connection) {
-        this.streamCreatedAt = null;
-        this.stream = null;
-        this.user = null
-        this.request = {
-          config: {
-            encoding: 'LINEAR16',
-            sampleRateHertz: 8000,
-            languageCode: 'en-UK' //Default Lang, will be updated with value from websocket
-          },
-          interimResults: false // If you want interim results, set this to true
-        };
-        connection.on('message', this.processMessage.bind(this));
-        connection.on('close', this.close.bind(this));
-    }
-
-    processMessage(message){
-        if (message.type === 'utf8') {
-            // Log the initial Message
-            var data = JSON.parse(message.utf8Data)
-            this.request.config.languageCode = data.languageCode
-            this.user = data.user
-        }
-        else if (message.type === 'binary') {
-          this.getStream().write(message.binaryData);
-        }
-    }
-
-    close(){
-        this.stream.destroy();
-    }
-
-    newStreamRequired() {
-        // No stream exists
-        if(!this.stream) {
-            return true;
-        }
-        // check time since stream was created.  If 60+ seconds ago create a new stream
-        else {
-            const now = new Date();
-            const timeSinceStreamCreated = (now - this.streamCreatedAt); // returns millis since stream created
-            return (timeSinceStreamCreated/1000) > 60;
-        }
-    }
-
-    // helper function to ensure we always get a stream object with enough time remaining to work with
-    getStream() {
-        if(this.newStreamRequired()) {
-            if (this.stream){
-                this.stream.destroy();
-            }
-            this.streamCreatedAt = new Date();
-            //console.log("Sending request as " + this.request.config.languageCode);
-            this.stream = speech.streamingRecognize(this.request)
-            .on('error', console.error)
-            .on('data', this.sendTranscription.bind(this));
-        }
-        return this.stream;
-    }
-
-    sendTranscription(data){
-        var response = {text: data.results[0].alternatives[0].transcript,
-                        languageCode: this.request.config.languageCode,
-                        user: this.user
-                        }
-        console.log(response);
-        //send to client haha lmao
-    }
-}
+// 
+// var WebSocketServer = require('websocket').server;
+// var url = require("url");
+// const fs = require('fs');
+// const Speech = require('@google-cloud/speech');
+// const speech = new Speech.SpeechClient();
+//
+// //Create a server
+// var httpServerNexmo = http.createServer(
+//   function (req, res) {
+//     // do nothing
+//     // res.writeHead(200, {'Content-Type': 'text/plain'});
+//     // res.write('Hello World!');
+//     // res.end();
+//   }
+// );
+//
+// httpServerNexmo.listen(port + 1, function () {
+//   console.log('httpServerNexmo on port: ' + (port + 1));
+// });
+//
+// var nexmows = new WebSocketServer({
+//     httpServer: httpServerNexmo,
+//     autoAcceptConnections: true,
+// });
+//
+// // Receive Recording
+// app.post(EVENT_PATH_RECORDING, function(req, res) {
+//     var parsedUrl = url.parse(req.url, true); // true to get query as object
+//     var getparams = parsedUrl.query;
+//     var params = JSON.parse(req.body);
+//     console.log(req.body)
+//     var localfile = "files/"+params['conversation_uuid']+".wav"
+//     nexmo.files.save(params['recording_url'], localfile, (err, response) => {
+//       if(response) {
+//           console.log('The audio is downloaded successfully!');
+//           var response = {text: "http://https://sound-machine-234713.appspot.com/" + localfile,
+//                           languageCode: getparams.langCode,
+//                           user: getparams.from
+//                           }
+//       }
+//     });
+//     res.writeHead(204);
+//     res.end();
+// });
+//
+// // Nexmo Websocket Handler
+// nexmows.on('connect', function(connection) {
+//     console.log((new Date()) + ' Connection accepted' + ' - Protocol Version ' + connection.webSocketVersion);
+//     // Create the stream at the start of the call
+//     var recognizeStream = new RecognizeStream(connection);
+// });
+//
+// class RecognizeStream {
+//     constructor(connection) {
+//         this.streamCreatedAt = null;
+//         this.stream = null;
+//         this.user = null
+//         this.request = {
+//           config: {
+//             encoding: 'LINEAR16',
+//             sampleRateHertz: 8000,
+//             languageCode: 'en-UK' //Default Lang, will be updated with value from websocket
+//           },
+//           interimResults: false // If you want interim results, set this to true
+//         };
+//         connection.on('message', this.processMessage.bind(this));
+//         connection.on('close', this.close.bind(this));
+//     }
+//
+//     processMessage(message){
+//         if (message.type === 'utf8') {
+//             // Log the initial Message
+//             var data = JSON.parse(message.utf8Data)
+//             this.request.config.languageCode = data.languageCode
+//             this.user = data.user
+//         }
+//         else if (message.type === 'binary') {
+//           this.getStream().write(message.binaryData);
+//         }
+//     }
+//
+//     close(){
+//         this.stream.destroy();
+//     }
+//
+//     newStreamRequired() {
+//         // No stream exists
+//         if(!this.stream) {
+//             return true;
+//         }
+//         // check time since stream was created.  If 60+ seconds ago create a new stream
+//         else {
+//             const now = new Date();
+//             const timeSinceStreamCreated = (now - this.streamCreatedAt); // returns millis since stream created
+//             return (timeSinceStreamCreated/1000) > 60;
+//         }
+//     }
+//
+//     // helper function to ensure we always get a stream object with enough time remaining to work with
+//     getStream() {
+//         if(this.newStreamRequired()) {
+//             if (this.stream){
+//                 this.stream.destroy();
+//             }
+//             this.streamCreatedAt = new Date();
+//             //console.log("Sending request as " + this.request.config.languageCode);
+//             this.stream = speech.streamingRecognize(this.request)
+//             .on('error', console.error)
+//             .on('data', this.sendTranscription.bind(this));
+//         }
+//         return this.stream;
+//     }
+//
+//     sendTranscription(data){
+//         var response = {text: data.results[0].alternatives[0].transcript,
+//                         languageCode: this.request.config.languageCode,
+//                         user: this.user
+//                         }
+//         console.log(response);
+//         //send to client haha lmao
+//     }
+// }
 
 //----------------------------------------------------------------------------
 //                            Test
